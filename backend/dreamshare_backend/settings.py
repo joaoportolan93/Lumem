474c2b28c94a6a10a392da089fe1909e58eceac6
joaoportolan93 <joaoportolan93@gmail.com>
2026-03-22 22:40:19 -0300

    seila

--- backend/dreamshare_backend/settings.py
+++ backend/dreamshare_backend/settings.py
@@ -153,6 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.Usuario'
 
 # CORS Configuration
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
