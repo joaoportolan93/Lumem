de0e0346b1ae60ded23d2d87718e61b33ff4033f
JacquesDMuller <jacquesdmuller93@gmail.com>
2026-03-27 19:34:56 -0300

    fix: renomeia referências lumem_backend para dreamshare_backend

--- backend/dreamshare_backend/asgi.py
+++ backend/dreamshare_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lumem_backend project.
+ASGI config for dreamshare_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lumem_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dreamshare_backend.settings')
 
 application = get_asgi_application()
